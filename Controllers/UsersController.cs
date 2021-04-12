f8fca07eec71d6ff9660ab779f605399f63f2ef0
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-12 13:46:17 -0600

    Add files via upload

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS132MyGram.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CS132MyGram.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult LogUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult LogAUser([FromForm]User user)
+        {
+            string result = $"The user name is {user.UserName}" +
+                            $"and password is: {user.Password}";
+            return Content(result);
+        }
+    }
+}
