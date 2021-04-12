fef4cb2ad14aef71e3568abb29dfa04499bc4151
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-12 12:05:36 -0600

    Add files via upload

---
+++ Models/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CS132MyGram.Models
+{
+    public class Image
+    {
+        public Image()
+        {
+        }
+        public int ImageId { get; set; }
+        public string ImageName { get; set; }
+        public string ImagePath { get; set; }
+        public string ImageDescription { get; set; }
+        public string ImageAltText { get; set; }
+        
+    }
+}
