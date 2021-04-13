98b0d4b0d6a35e7fcb8cb2b9b580f7670c3862f1
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-13 11:23:41 -0600

    Add files via upload

---
+++ MyGramDatabase.sql
@@ -0,0 +1,15 @@
+
+
+Create Table Image(
+     ImageId int,
+     ImageName varchar(500),
+     ImagePath varchar(500),
+     ImageDescription varchar(500),
+     ImageAltText varchar(500),
+);
+
+Alter Table Image
+Add CreatedbyId INT;
+
+
+
