d8884d5f556fcb5b893265ecce871dd2e18ef77b
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-19 12:17:18 -0600

    Update MyGramDatabase.sql

--- MyGramDatabase.sql
+++ MyGramDatabase.sql
@@ -8,8 +8,128 @@ Create Table Image(
      ImageAltText varchar(500),
 );
 
-Alter Table Image
-Add CreatedbyId INT;
 
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(1,"Image1","ImagePath1","ImageDescription1","ImageAltText1",1,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(2,"Image2","ImagePath2","ImageDescription2","ImageAltText2",2,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(3,"Image3","ImagePath3","ImageDescription3","ImageAltText3",3,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(4,"Image4","ImagePath4","ImageDescription4","ImageAltText4",4,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(5,"Image5","ImagePath5","ImageDescription5","ImageAltText5",5,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(6,"Image6","ImagePath6","ImageDescription6","ImageAltText6",6,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(7,"Image7","ImagePath7","ImageDescription7","ImageAltText7",7,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(8,"Image8","ImagePath8","ImageDescription8","ImageAltText8",8,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(9,"Image9","ImagePath9","ImageDescription9","ImageAltText9",9,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(10,"Image10","ImagePath10","ImageDescription10","ImageAltText10",10,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(11,"Image11","ImagePath11","ImageDescription11","ImageAltText11",11,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(12,"Image12","ImagePath12","ImageDescription12","ImageAltText12",12,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(13,"Image13","ImagePath13","ImageDescription13","ImageAltText13",13,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(14,"Image14","ImagePath14","ImageDescription14","ImageAltText14",14,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(15,"Image15","ImagePath15","ImageDescription15","ImageAltText15",15,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(16,"Image16","ImagePath16","ImageDescription16","ImageAltText16",16,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(17,"Image17","ImagePath17","ImageDescription17","ImageAltText17",17,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(18,"Image18","ImagePath18","ImageDescription18","ImageAltText18",18,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(19,"Image19","ImagePath19","ImageDescription19","ImageAltText19",19,curdate());
+
+insert into Image
+(ImageId,ImageName,ImagePath,ImageDescription,ImageAltText,
+CreatedById,CreatedOn )
+Values
+(20,"Image20","ImagePath20","ImageDescription20","ImageAltText20",20,curdate());
+
+Select * From
+Image;
 
 
