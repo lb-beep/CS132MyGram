85455280fc3dfe3f397085961f68e89b35bfdada
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-21 11:15:19 -0600

    Add files via upload

---
+++ Models/SignUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CS132MyGram.Models
+{
+    public class SignUp
+    {
+        public SignUp()
+        {
+
+        }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int dateOfBirth { get; set; }
+        public string gender { get; set; }
+    }
+}
