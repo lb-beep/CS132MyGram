66a4029abebcd125aaad5e1a434d8f47f68ad2c2
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-21 11:14:06 -0600

    Add files via upload

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS132MyGram.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CS132MyGram.Controllers
+{
+    public class SignUpController : Controller
+    {
+        private object firstName;
+        private object lastName;
+        private object dateOfBirth;
+        private object gender;
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SignUp([FromForm]SignUp signUp)
+        {
+            string result = $"The user name is: {firstName} {lastName}" +
+                $"they were born on: {dateOfBirth}" +
+                $"the gender is {gender}"; 
+
+            return Content(result);
+        }
+
+    }
+}
+
