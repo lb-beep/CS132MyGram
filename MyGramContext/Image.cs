8f9422b9e9de1edd3b927b10f93e20cd1aac43d5
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-05-05 14:20:01 -0600

    Add files via upload

---
+++ MyGramContext/Image.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using CS132MyGram.Models;
+
+#nullable disable
+
+namespace CS132MyGram.MyGramContext
+{
+    public partial class Image
+    {
+        public Image()
+        {
+            ProductImage = new HashSet<ProductImage>();
+        }
+
+        public int ImageId { get; set; }
+        public string ImageName { get; set; }
+        public string ImagePath { get; set; }
+        public string ImageDescription { get; set; }
+        public string ImageAltText { get; set; }
+        public int CreatedById { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public virtual ICollection<ProductImage> Image { get; set; }
+
+    }
+
+    internal class ProductImage
+    {
+    }
+}
---
+++ MyGramContext/MYGRAMCONTEXT.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using CS132MyGram.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+namespace CS132MyGram.MyGramContext
+{
+    public partial class MYGRAMCONTEXT : DbContext
+    {
+        public MYGRAMCONTEXT()
+        {
+        }
+
+        public MYGRAMCONTEXT(DbContextOptions<MYGRAMCONTEXT> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Image> ImageId { get; set; }
+        public virtual DbSet<Image> ImageName { get; set; }
+        public virtual DbSet<Image> ImagePath { get; set; }
+        public virtual DbSet<Image> ImageDescription { get; set; }
+        public virtual DbSet<Image> ImageAltText { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Image>(entity =>
+            {
+
+                entity.ToTable("IMAGE", "MYGRAM");
+
+                entity.HasIndex(e => e.ImageName, "ImageName").IsUnique();
+
+                entity.Property(e => e.ImageId).HasDefaultValueSql("'10'");
+
+                entity.Property(e => e.ImageDescription)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .HasDefaultValueSql("'DESCRIPTION TEST'");
+
+
+                entity.Property(e => e.ImageName)
+                    .IsRequired()
+                    .HasMaxLength(500)
+                    .HasDefaultValueSql("'PRODUCT NAME TEST'");
+
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder); 
+
+
+    }
+}
